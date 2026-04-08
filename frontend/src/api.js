345cb86c7ff97f32f332869cadd61275d420ee1d
Sourcein Admin <admin@example.com>
2026-04-08 20:15:08 +0530

    fix: updated API_ROOT for production and added license

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_ROOT = 'http://localhost:5000/api';
+const API_ROOT = import.meta.env.MODE === 'production' 
+  ? '/api' 
+  : 'http://localhost:5000/api';
 
 export const loginUser = async (email) => {
   const { data } = await axios.post(`${API_ROOT}/users/auth`, { email });
